70733e671a8ccd0989130fdc98eccefde3e0472f
Carlos de Leon <admin@koxland.net>
2025-06-12 17:30:22 -0600

    Update CouponCardV3.jsx

--- src/components/cards/CouponCardV3.jsx
+++ src/components/cards/CouponCardV3.jsx
@@ -8,7 +8,7 @@ function CouponCardV3({ name, valid, promo, categorie, details, logo }) {
   });
   return (
     <div className="relative flex h-[353px] w-[345px] flex-col justify-between rounded-2xl border border-neutral-700 bg-white px-8 py-6 shadow-lg">
-      {/* Parte superior (logo + texto) */}
+      {/* Logo and Text */}
       <div className="flex flex-grow flex-col items-center justify-start gap-3">
         <div className="flex h-[90px] items-center justify-center">
           <img
@@ -27,7 +27,7 @@ function CouponCardV3({ name, valid, promo, categorie, details, logo }) {
         </div>
       </div>
 
-      {/* Línea y círculos */}
+      {/* Line and Circles */}
       <div className="relative mt-2 h-6 w-full">
         <div className="absolute inset-0 h-[1px] w-full bg-[linear-gradient(to_right,_#8a8888_4px,_transparent_4px)] bg-[length:8px_1px] bg-repeat-x" />
         <div
@@ -40,7 +40,7 @@ function CouponCardV3({ name, valid, promo, categorie, details, logo }) {
         />
       </div>
 
-      {/* Botón */}
+      {/* Button */}
       <div className="flex w-full items-center justify-center pt-3">
         <button className="cursor-pointer rounded-[42px] border bg-[#297da9] px-12 py-4 text-base font-semibold text-white transition-colors duration-300 hover:bg-[#004165] active:bg-[#004165]">
           View Details
