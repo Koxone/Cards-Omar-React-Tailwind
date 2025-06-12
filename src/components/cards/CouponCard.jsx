ecfa67f3d14cfbaeba2f8ea313c119ca5b4b6d64
Carlos de Leon <admin@koxland.net>
2025-06-12 15:59:40 -0600

    Update CouponCard.jsx

--- src/components/cards/CouponCard.jsx
+++ src/components/cards/CouponCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function CouponCard({ name, validUntil, discount, logo }) {
   return (
-    <div className="relative flex min-h-[104px] w-[345px] cursor-pointer flex-col items-center gap-4 rounded-[8px] border border-neutral-700 bg-white px-4 py-4 shadow-xl">
+    <div className="relative flex max-h-[352px] min-h-[104px] w-[345px] cursor-pointer flex-col items-center gap-4 rounded-[8px] border border-neutral-700 bg-white px-4 py-4 shadow-xl">
       <div className="absolute top-[50%] left-[-6%] h-8 w-8 translate-y-[-50%] rounded-full bg-[#004466]" />
       <div className="absolute top-[50%] right-[-6%] h-8 w-8 translate-y-[-50%] rounded-full bg-[#004466]" />
 
