6ec6f257d04c5c328ce300b172505363fe8e2782
Carlos de Leon <admin@koxland.net>
2025-06-13 21:40:57 -0600

    c

--- src/components/cards/CouponCardV3.jsx
+++ src/components/cards/CouponCardV3.jsx
@@ -19,19 +19,32 @@ function CouponCardV3({
     <div className="max-h-[353px] max-w-[345px]">
       <div className="grid grid-rows-[200px_50px_1fr] rounded-2xl border border-[#297da9] shadow-2xl">
         <div className="flex flex-col items-center justify-center p-4 text-center">
-          <img src="/assets/mcdonalds.png" alt="" />
+          <img src={logo} alt="" />
           <h2 className="text-3xl leading-tight font-bold tracking-widest break-words text-[#297da9] uppercase">
-            20% OFF Family Buckets
+            {promo}
           </h2>
           <p className="font-medium tracking-wider text-[#297da9]">Fast food</p>
         </div>
-        <div className="z-50 flex flex-row items-center bg-[#f9f9f9]">
+        <div className="flex flex-row items-center bg-[#f9f9f9]">
           <div className="h-[28px] w-[14px] rounded-tl-[0px] rounded-tr-[14px] rounded-br-[14px] rounded-bl-[0px] border-r-1 border-[#297da9] bg-[#f9f9f9]"></div>
           <div className="h-[1px] w-full border-b border-dashed border-b-[rgba(204,222,240,0.6)]"></div>
           <div className="h-[28px] w-[14px] rounded-tl-[14px] rounded-tr-[0px] rounded-br-[0px] rounded-bl-[14px] border-l-1 border-[#297da9] bg-[#f9f9f9]"></div>
         </div>
         <div className="flex w-full items-center justify-center p-4 pt-3">
-          <button className="cursor-pointer rounded-[42px] border bg-[#297da9] px-12 py-4 text-base font-semibold text-white transition-colors duration-300 hover:bg-[#004165] active:bg-[#004165]">
+          <button
+            onClick={() =>
+              onClick({
+                promo,
+                name,
+                logo,
+                conditions,
+                categorie,
+                valid,
+                description,
+              })
+            }
+            className="cursor-pointer rounded-[42px] border bg-[#297da9] px-12 py-4 text-base font-semibold text-white transition-colors duration-300 hover:bg-[#004165] active:bg-[#004165]"
+          >
             View Details
           </button>
         </div>
