abd2858f69849a94405b9bcb0301eff41682e01b
Carlos de Leon <admin@koxland.net>
2025-06-17 15:07:49 -0600

    Update Modal.jsx

--- src/components/feedback/Modal.jsx
+++ src/components/feedback/Modal.jsx
@@ -48,7 +48,7 @@ function Modal({
       <div
         onClick={(e) => e.stopPropagation()}
         style={{ backgroundColor: bgColor }}
-        className="fixed top-1/2 left-1/2 z-20 flex h-[calc(100vh-140px)] w-[calc(100%-40px)] -translate-x-1/2 -translate-y-1/2 transform flex-col gap-6 rounded-2xl border border-[#297da9] p-8 shadow-2xl sm:h-[750px] sm:w-[550px]"
+        className="fixed top-1/2 left-1/2 z-20 flex w-[calc(100%-40px)] -translate-x-1/2 -translate-y-1/2 transform flex-col gap-6 rounded-2xl border border-[#297da9] p-8 shadow-2xl sm:w-[550px]"
       >
         <button
           onClick={onClose}
@@ -67,7 +67,7 @@ function Modal({
 
         {/* Title, Categorie and Logo */}
         <div className="flex h-[121px] items-center justify-between pt-4">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
             <p className="text-2xl leading-7 font-semibold tracking-wide text-[#024466]">
               {name}
             </p>
@@ -77,22 +77,20 @@ function Modal({
         </div>
 
         {/* Description and Apply Button */}
-        <div className="flex h-full flex-col">
+        <div className="flex h-full flex-col gap-4">
           <div className="mb-auto flex flex-col gap-4">
             <p className="font-semibold tracking-wider md:text-2xl">
               {t("modal.description")}
             </p>
-            <div>
-              <p className="text-[11pt] font-medium tracking-wide text-neutral-500 md:text-base md:leading-9">
-                {description[lang]}
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center gap-4">
-            <p className="text-xs font-medium text-[#297da9] underline underline-offset-2 transition-colors duration-200 md:text-sm">
+            <p className="text-[11pt] font-medium tracking-wide text-neutral-500 md:text-base md:leading-9">
+              {description[lang]}
+            </p>
+            <p className="self-center text-xs font-medium text-[#297da9] underline underline-offset-2 transition-colors duration-200 md:text-sm">
               Terms & Conditions with provider
             </p>
+          </div>
 
+          <div className="flex flex-col items-center gap-4">
             {/* Wallet Buttons */}
             <div className="flex w-full items-center justify-between pt-2 sm:justify-evenly">
               <button
@@ -110,7 +108,7 @@ function Modal({
                 }}
               >
                 <img
-                  className="w-[130px cursor-pointer md:h-[50px] md:w-[160px]"
+                  className="cursor-pointer md:h-[50px] md:w-[160px]"
                   src="/assets/apple.svg"
                   alt="Apple Wallet"
                 />
