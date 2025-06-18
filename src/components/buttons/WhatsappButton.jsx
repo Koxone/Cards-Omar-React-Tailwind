bb92bf71a14a9d3c840f730b90281839d162b19c
Carlos de Leon <admin@koxland.net>
2025-06-18 13:51:50 -0600

    Update WhatsappButton.jsx

--- src/components/buttons/WhatsappButton.jsx
+++ src/components/buttons/WhatsappButton.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function WhatsappButton() {
   return (
     <a
+      target="_blank"
       className="flex h-10 w-fit cursor-pointer items-center justify-center gap-1 rounded-sm border bg-[#16161d] px-2 text-center text-[7px] font-semibold text-white uppercase transition-colors duration-300 hover:bg-[#004165] md:h-[40px] md:w-fit md:px-3 md:text-sm"
       href="https://api.whatsapp.com/send?phone=+529981687548&text=Hello"
     >
